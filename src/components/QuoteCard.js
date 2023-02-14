87fa792a3ff503f9c890657480260ba52bd7c4d9
Elea641 <(mailto:eleavoliotis64@gmail.com)>
2023-02-14 09:36:13 +0100

    quotes

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,21 +1,22 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import './QuoteCard.css';
 
-function QuoteCard() {
+function QuoteCard(props) {
   return (
-    <figure class="QuoteCard">
-      <img
-        src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185"
-        alt="Nelson Muntz"
-      />
+    <figure className="QuoteCard">
+      <img src={props.image} alt={props.character} />
       <figcaption>
-        <blockquote>
-          Shoplifting is a victimless crime, like punching someone in the dark.
-        </blockquote>
-        <cite>Nelson Muntz</cite>
+        <blockquote>{props.quote}</blockquote>
+        <cite>{props.character}</cite>
       </figcaption>
     </figure>
   );
 }
+
+QuoteCard.propTypes = {
+  characterFirstName: PropTypes.string.isRequired,
+  characterLastName: PropTypes.string.isRequired,
+};
 
 export default QuoteCard;
